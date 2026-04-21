ead4fe769da1a296625db799cc6b2bbb388ff22c
pixelated-pup <acfrostie@gmail.com>
2026-04-21 16:56:47 +1000

    PWM LED

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ use stm32g4xx_hal::{
     pwr::PwrExt,
     rcc::Config,
     stm32,
-    time::ExtU32,
+    time::{ExtU32, RateExtU32},
     timer::Timer,
 };
 
@@ -25,27 +25,40 @@ fn main() -> ! {
     let mut rcc = dp.RCC.freeze(Config::hsi(), pwr);
 
     let gpioa = dp.GPIOA.split(&mut rcc);
+    let gpiob = dp.GPIOB.split(&mut rcc);
 
-    let mut led = gpioa.pa5.into_push_pull_output();
-
-    let timer2 = Timer::new(dp.TIM2, &rcc.clocks);
-    let mut delay_tim2 = timer2.start_count_down(100.millis()).delay();
-
-    let analog_pin = gpioa.pa7.into_analog();
-    let adc12_common = dp.ADC12_COMMON.claim(ClockMode::default(), &mut rcc);
-    let mut adc = adc12_common.claim_and_configure(
-        dp.ADC2,
-        AdcConfig::default(),
-        &mut cp.SYST.delay(&rcc.clocks),
+    let led_pins = (
+        gpioa.pa0.into_alternate(),
+        gpioa.pa1.into_alternate(),
+        gpiob.pb10.into_alternate(),
     );
+    let (mut c1, mut c2, mut c3) = dp.TIM2.pwm(led_pins, 100.Hz(), &mut rcc);
+
+    let timer = Timer::new(dp.TIM1, &rcc.clocks);
+    let mut delay_timer = timer.start_count_down(100.millis()).delay();
+
+    // let analog_pin = gpioa.pa7.into_analog();
+    // let adc12_common = dp.ADC12_COMMON.claim(ClockMode::default(), &mut rcc);
+    // let mut adc = adc12_common.claim_and_configure(
+    //     dp.ADC2,
+    //     AdcConfig::default(),
+    //     &mut cp.SYST.delay(&rcc.clocks),
+    // );
 
     loop {
-        let sample = adc.convert(&analog_pin, SampleTime::Cycles_640_5);
-        let millivolts = adc.sample_to_millivolts(sample);
+        // let sample = adc.convert(&analog_pin, SampleTime::Cycles_640_5);
+        // let millivolts = adc.sample_to_millivolts(sample);
 
-        led.toggle();
-        delay_tim2.delay_ms(1000);
-        led.toggle();
-        delay_tim2.delay_ms(1000);
+        c1.set_duty_cycle_percent(1);
+        c2.set_duty_cycle_percent(0);
+        c3.set_duty_cycle_percent(0);
+
+        delay_timer.delay_ms(1000);
+
+        c1.set_duty_cycle_percent(0);
+        c2.set_duty_cycle_percent(0);
+        c3.set_duty_cycle_percent(0);
+
+        delay_timer.delay_ms(1000);
     }
 }
